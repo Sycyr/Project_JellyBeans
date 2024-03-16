d6f959983298e18432a0ff70e08c993e7c8fdb7f
Sycry <fabriciocima101@gmail.com>
2024-03-16 10:33:54 -0400

    start project

---
+++ app/src/main/java/br/com/app1/jellybean/MainActivity.java
@@ -0,0 +1,40 @@
+package br.com.app1.jellybean;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edtNome;
+    Button btnSaudacao;
+    TextView tvMensagem;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtNome = findViewById(R.id.editNome);
+        btnSaudacao = findViewById(R.id.btnBotao);
+        tvMensagem = findViewById(R.id.tvNome);
+
+        btnSaudacao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //pegar a info do edittext
+                String nome = edtNome.getText().toString();
+                String mensagem = "Olá " + nome;
+                tvMensagem.setText(mensagem);
+
+            }
+        });
+
+
+    }
+}
